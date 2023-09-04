1bd5e36c06c7758ba295668759bb3b6e1b06c37f
ksubin <ksubin1017@gmail.com>
2023-09-04 16:56:23 +0900

    문화, 뷰터 mapping

--- src/main/java/com/far/controller/MainController.java
+++ src/main/java/com/far/controller/MainController.java
@@ -6,28 +6,49 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-		
-	
-	// index 페이지를 나타냄
-	@RequestMapping("/")
-	public ModelAndView index() {
-		ModelAndView mav = new ModelAndView("main/index");
-		return mav;	
-	}
-	
-	// 로그인 페이지 이동
-	@RequestMapping("/login")
-	public ModelAndView login() {
-		ModelAndView mav = new ModelAndView("login/login");
-		return mav;
-	}
-	
-	// 고객센터 이동
-	@RequestMapping("/customer_service")
-	public ModelAndView customer_service() {
-		return new ModelAndView("cs/cs_main");
-	}
-	
-	// 마이페이지 
-	
+      
+   
+   // index 페이지를 나타냄
+   @RequestMapping("/")
+   public ModelAndView index() {
+      ModelAndView mav = new ModelAndView("main/index");
+      return mav;   
+   }
+   
+   // 로그인 페이지 이동
+   @RequestMapping("/login")
+   public ModelAndView login() {
+      ModelAndView mav = new ModelAndView("login/login");
+      return mav;
+   }
+   
+   // 고객센터 이동
+   @RequestMapping("/customer_service")
+   public ModelAndView customer_service() {
+      return new ModelAndView("cs/cs_main");
+   }
+   
+   // 마이페이지 
+   @RequestMapping("/my_page")
+   public ModelAndView my_page() {
+      return new ModelAndView("myPage/myPage");
+   }
+   
+   // 회원가입 
+   @RequestMapping("/signUp")
+   public ModelAndView signUp() {
+      return new ModelAndView("login/signUp");
+   }
+   
+   // 아이디 찾기
+   @RequestMapping("/findId")
+   public ModelAndView findId() {
+      return new ModelAndView("login/findId");
+   }
+   
+   // 비밀번호 찾기
+   @RequestMapping("/findPwd")
+   public ModelAndView findPwd() {
+      return new ModelAndView("login/findPwd");
+   }
 }
