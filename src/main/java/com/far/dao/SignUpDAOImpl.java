67e8dac06e8a104a6325e860d390ccacb2f281fd
ksubin <ksubin1017@gmail.com>
2023-09-10 21:46:19 +0900

    수빈 0910 회원가입

--- src/main/java/com/far/dao/SignUpDAOImpl.java
+++ src/main/java/com/far/dao/SignUpDAOImpl.java
@@ -7,11 +7,11 @@ import org.springframework.stereotype.Repository;
 import com.far.dto.MemberDTO;
 
 @Repository
-public class MemberDAOImpl implements MemberDAO {
-
+public class SignUpDAOImpl implements SignUpDAO {
+	
 	@Autowired
 	private SqlSession sqlSession;
-
+	
 	@Override
 	public void insertMember(MemberDTO m) {
 		sqlSession.insert("mem_in", m);
--- src/main/java/com/far/dto/MemberDTO.java
+++ src/main/java/com/far/dto/MemberDTO.java
@@ -20,10 +20,39 @@ public class MemberDTO {
 	
 	private String mem_id;
 	private String mem_pwd;
-	private String mem_email_id;
-	private String mem_email_domain;
+	
+	private String mem_name;
+	private String mem_email;
 	private String mem_tel;
-	private String mem_class;
-	private String reg_date;
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_tel() {
+		return mem_tel;
+	}
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
 	
 }
