1935db200e4703264564702509c346f8d1702cb8
ksubin <ksubin1017@gmail.com>
2023-09-13 23:22:58 +0900

    0913 수빈 Security

--- src/main/java/com/far/dto/ResvDTO.java
+++ src/main/java/com/far/dto/ResvDTO.java
@@ -18,10 +18,45 @@ import lombok.Getter;
 public class ResvDTO {
 	
 	private int resv_num;
+	public int getResv_num() {
+		return resv_num;
+	}
+	public void setResv_num(int resv_num) {
+		this.resv_num = resv_num;
+	}
+	public int getStore_num() {
+		return store_num;
+	}
+	public void setStore_num(int store_num) {
+		this.store_num = store_num;
+	}
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getStart_day() {
+		return start_day;
+	}
+	public void setStart_day(String start_day) {
+		this.start_day = start_day;
+	}
+	public String getEnd_day() {
+		return end_day;
+	}
+	public void setEnd_day(String end_day) {
+		this.end_day = end_day;
+	}
+	public int getPeople_num() {
+		return people_num;
+	}
+	public void setPeople_num(int people_num) {
+		this.people_num = people_num;
+	}
 	private int store_num;
 	private String mem_id;
 	private String start_day;
 	private String end_day;
 	private int people_num;
-	
 }
---
+++ src/main/java/com/far/model/Member.java
@@ -0,0 +1,58 @@
+package com.far.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Entity
+public class Member {
+	
+	@Id
+	//@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private String mem_id;
+	private String mem_pwd;
+	private String mem_name;
+	private String mem_email;
+	private String mem_tel;
+	private String mem_class;
+	
+	public String getMem_id() {
+		return mem_id;
+	}
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+	public String getMem_name() {
+		return mem_name;
+	}
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+	public String getMem_email() {
+		return mem_email;
+	}
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+	public String getMem_tel() {
+		return mem_tel;
+	}
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
+	public String getMem_class() {
+		return mem_class;
+	}
+	public void setMem_class(String mem_class) {
+		this.mem_class = mem_class;
+	}
+}
