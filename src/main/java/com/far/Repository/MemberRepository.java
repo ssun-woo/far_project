31fad3b6e67768bf2ad8a98fff0d1729f8b77bce
ksubin <ksubin1017@gmail.com>
2023-09-22 19:57:04 +0900

    0922 수빈 아이디찾기

--- src/main/java/com/far/Repository/MemberRepository.java
+++ src/main/java/com/far/Repository/MemberRepository.java
@@ -5,4 +5,8 @@ import com.far.model.Member;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
 	public Member findByMemId(String memId);
+	public Member findByMemNameAndMemEmail(String memName, String memEmail);
+	public Member findByMemNameAndMemTel(String memName, String memTel);
+	public Member findByMemIdAndMemNameAndMemEmail(String memId, String memName, String memEmail);
+	public Member findByMemIdAndMemNameAndMemTel(String memId, String memName, String memTel);
 }
--- src/main/java/com/far/controller/LoginController.java
+++ src/main/java/com/far/controller/LoginController.java
@@ -1,5 +1,8 @@
 package com.far.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -7,16 +10,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.far.model.Member;
+import com.far.service.FindIdService;
 import com.far.service.LoginService;
+import com.far.service.MemberService;
 
 @Controller
 public class LoginController {
 	@Autowired
 	private LoginService loginService;
 	
+	@Autowired
+	private FindIdService findIdService;
 	// 로그인 폼으로 이동
 	@GetMapping("/loginForm")
 	public String loginForm() {
@@ -31,9 +41,24 @@ public class LoginController {
 		return "redirect:/loginForm";
 	}
 	
-	// 아이디 찾기
-	@PostMapping("/findIdEmail")
-	public String findIdEmail() {
-		return "login/findIdEmail";
+	@PostMapping("/findId")
+	public String findIdEmail(String memName, String emailortel, Model model) {
+	    // "@" 기호가 포함되어 있는지 확인
+	    if (emailortel.matches("(.*)@(.*)")) {
+	        // "@" 기호가 포함되어 있다면 아이디 찾기 로직 수행
+	        Member m = findIdService.findIdEmail(memName, emailortel);
+	        model.addAttribute("m", m);
+	        return "login/findIdresult";
+	    } else {
+	        // "@" 기호가 포함되어 있지 않으면 오류 메시지 반환 또는 다른 처리 수행
+	    	Member m = findIdService.findIdTel(memName, emailortel);
+	        model.addAttribute("m", m);
+	        System.out.println("memberId: " + m.getMemId());
+	        return "login/findIdresult";
+	    }
 	}
+	
+	
+
+	
 }
